8a851dc485da02cd22ca3b076b710965b330ab32
amenos2014 <hus13th@hotmail.com>
2024-10-14 20:16:54 +0300

    Update and rename amenos to amenos.py

---
+++ amenos.py
@@ -0,0 +1,7 @@
+import random 
+
+
+gerçek = ["yüzmeyi severim,","en sevdiğim tatlı şeker pare","geçekten sosyalim ","sınıf öğretmenimin kedisinin ismi zarife "]
+
+rasgele_bilgi = random.choice(gerçek)
+print ("hakkımdaki gerçek:",rasgele bilgi)
